bc2bac71a545124c9c8c6c0bdf45d680898436bc
polin103109 <sabrinpolinece18@gmail.com>
2023-05-10 00:42:46 +0600

    edited readme file

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-E-Commerce-Site-
-
+<h1>E-Commerce-React <h1>
+Live Link:https://sabrinaproject.pages.dev/
 <!--  -->
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../Header/Header';
 import { Outlet } from 'react-router-dom';
+import Shop from '../Shop/Shop';
 const Home = () => {
     return (
         <div>
         <Header></Header>
         <Outlet></Outlet>
+        <Shop></Shop>
         </div>
     )
 }
